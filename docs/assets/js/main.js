d66d5e4dbde0a2272488aaee8b423c191e02b224
yaocoo <yaocong.duan@gmail.com>
2017-12-19 01:09:26 +0000

    update

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -7,7 +7,6 @@ var isFF = userAgent.indexOf("Firefox") > -1; // judge if Firefox
 var isSafari = userAgent.indexOf("Safari") > -1 && userAgent.indexOf("Chrome") == -1; // judge if Safari
 var isChrome = userAgent.indexOf("Chrome") > -1 && userAgent.indexOf("Safari") > -1; // judge if Chrome
 
-var navbar = $("#site-nav");
 var topBtn = $("#topBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -21,66 +20,10 @@ function scrollFunction() {
     } else {
         topBtn.css("display", "none");
     }
-
-    if (isIE || isEdge) {
-        // IE & EDGE
-        navbar.removeClass("sticky-top");
-    } else {
-        if (navbar.offset().top - $(window).scrollTop() <= 1) {
-            navbar.addClass("bg-lightone");
-        } else {
-            navbar.removeClass("bg-lightone");
-        }
-    }
 }
 
 function topFunction() {
     document.body.scrollTop = 0; // For Chrome, Safari and Opera 
     document.documentElement.scrollTop = 0; // For IE and Firefox
 }
-
-/* For timeline
- –––––––––––––––––––––––––––––––––––––––––––––––––– */
-(function() {
-
-    'use strict';
-
-    // define variables
-    var items = document.querySelectorAll(".timeline li");
-
-    // check if an element is in viewport
-    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-    function isElementInViewport(el) {
-        var rect = el.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
-
-    function callbackFunc() {
-        for (var i = 0; i < items.length; i++) {
-            if (isElementInViewport(items[i])) {
-                items[i].classList.add("in-view");
-            }
-        }
-    }
-
-    // listen for events
-    window.addEventListener("load", callbackFunc);
-    window.addEventListener("resize", callbackFunc);
-    window.addEventListener("scroll", callbackFunc);
-
-})();
-
-/* For iframe automatically define Height
- –––––––––––––––––––––––––––––––––––––––––––––––––– */
-// function iframeLoad(frameId)  
-// {  
-//     frame = document.getElementById(frameId);
-//     frame.height=0;  
-//     frame.height=frame.contentWindow.document.body.scrollHeight;  
-// }  
 
--- docs/assets/js/open-search.js
+++ docs/assets/js/open-search.js
@@ -2,60 +2,74 @@ obj = getQueryString('obj');
 srchContent = getQueryString('srchContent');
 srchType = getQueryString('srchType');
 
-var googleScho = {
-    articleUrlPre: "https://scholar.google.com/scholar?hl=en&q=",
-    authorUrlPre: "https://scholar.google.com/citations?view_op=search_authors&hl=en&mauthors=",
-}
-var semanticScho = {
-    articleUrlPre: "https://www.semanticscholar.org/search?sort=relevance&q=",
-}
-
-var researchGate = {
-    articleUrlPre: "https://www.researchgate.net/search.Search.html?type=publication&query=",
-    authorUrlPre: "https://www.researchgate.net/search.Search.html?type=researcher&query=",
+articleUrlPre = {
+    googleScho: "https://scholar.google.com/scholar?hl=en&q=",
+    semanticScho: "https://www.semanticscholar.org/search?sort=relevance&q=",
+    researchGate: "https://www.researchgate.net/search.Search.html?type=publication&query=",
+    pubmed: "https://www.ncbi.nlm.nih.gov/pubmed/?term=",
+    sage: "http://journals.sagepub.com/action/doSearch?AllField=",
+    nature: "https://www.nature.com/search?q=",
+    science: "http://search.sciencemag.org/?q=",
+    cell: "http://www.cell.com/action/doSearch?searchType=quick&occurrences=all&journalCode=&searchScope=fullSite&searchText=",
 }
 
-var sage = {
-    articleUrlPre: "http://journals.sagepub.com/action/doSearch?AllField=",
+authorUrlPre = {
+    googleScho: "https://scholar.google.com/citations?view_op=search_authors&hl=en&mauthors=",
+    researchGate: "https://www.researchgate.net/search.Search.html?type=researcher&query=",
 }
 
 if (obj == 'article') {
     switch (srchType) {
         case 'google_scholar':
-            // srchContent = srchContent.replace('+',' ');
-            resultUrl = googleScho.articleUrlPre + srchContent;
+            resultUrl = articleUrlPre.googleScho + srchContent;
             break;
 
         case 'semantic_scholar':
             srchContent = srchContent.replace('+', ' ');
-            resultUrl = semanticScho.articleUrlPre + srchContent;
+            resultUrl = articleUrlPre.semanticScho + srchContent;
             break;
 
         case 'research_gate':
             srchContent = srchContent.replace('+', ' ');
-            resultUrl = researchGate.articleUrlPre + srchContent;
+            resultUrl = articleUrlPre.researchGate + srchContent;
+            break;
+
+        case 'pubmed':
+            resultUrl = articleUrlPre.pubmed + srchContent;
             break;
 
         case 'sage_journals':
-            // srchContent = srchContent.replace('+',' ');
-            resultUrl = sage.articleUrlPre + srchContent;
+            resultUrl = articleUrlPre.sage + srchContent;
             break;
+
+        case 'nature':
+            resultUrl = articleUrlPre.nature + srchContent;
+            break;
+
+        case 'science':
+            srchContent = srchContent.replace('+', ' ');
+            resultUrl = articleUrlPre.science + srchContent;
+            break;
+
+        case 'cell':
+            resultUrl = articleUrlPre.cell + srchContent;
+            break;
+
     }
 } else if (obj == 'author') {
     switch (srchType) {
         case 'google_scholar':
-            // srchContent = srchContent.replace('+',' ');
-            resultUrl = googleScho.authorUrlPre + srchContent;
+            resultUrl = authorUrlPre.googleScho + srchContent;
             break;
 
         case 'subject_aera':
             srchContent = srchContent.replace('+', '_');
-            resultUrl = googleScho.authorUrlPre + 'label:' + srchContent;
+            resultUrl = authorUrlPre.googleScho + 'label:' + srchContent;
             break;
 
         case 'research_gate':
             srchContent = srchContent.replace('+', ' ');
-            resultUrl = researchGate.authorUrlPre + srchContent;
+            resultUrl = authorUrlPre.researchGate + srchContent;
             break;
     }
 }
