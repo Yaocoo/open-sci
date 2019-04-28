e0188df858f612e39dbafd68ed4d0f442d0232dc
yaocoo <yaocong.duan@gmail.com>
2019-04-28 13:08:43 +0100

    update sci-hub server

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,14 +1,25 @@
 //  functions
 function changeScihubHost(host) {
     switch (host) {
+        case 'tw':
+            host_url = 'https://sci-hub.tw';
+            break;
+        case 'se':
+            host_url = 'https://sci-hub.se';
+            break;
+        case 'shop':
+            host_url = 'https://sci-hub.shop';
+            break;
+        case 'fun':
+            host_url = 'https//sci-hub.fun';
+            break;
+
+
         case 'cn':
             host_url = 'http://sci-hub.cn';
             break;
         case 'hk':
             host_url = 'http://sci-hub.hk';
-            break;
-        case 'tw':
-            host_url = 'http://sci-hub.tw';
             break;
         case 'tv':
             host_url = 'http://sci-hub.tv';
